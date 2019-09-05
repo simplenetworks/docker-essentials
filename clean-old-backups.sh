6e6fa4d5d367f5c092c091deb264b09b8b9571e8
SimpleNetworks <38137505+simplenetworks@users.noreply.github.com>
2019-09-05 12:10:34 +0200

    Update clean-old-backups.sh

--- clean-old-backups.sh
+++ clean-old-backups.sh
@@ -30,36 +30,37 @@ EOF
 lista=( $listing )
 
 # loop over our files
-for ((FNO=0; FNO<${#lista[@]}; FNO+=9));do
+for ((FNO = 0; FNO < ${#lista[@]}; FNO += 9)); do
   # month (element 5), day (element 6) and filename (element 8)
-  #echo Date ${lista[`expr $FNO+5`]} ${lista[`expr $FNO+6`]} File: ${lista[`expr $FNO+8`]}
-   # echo FNO $FNO
+  # echo Date ${lista[$(expr $FNO+5)]} ${lista[$(expr $FNO+6)]} File: ${lista[$(expr $FNO+8)]}
+  # echo FNO $FNO
   # check the date stamp
-   FILE_MONTH=$(date -d "${lista[`expr $FNO+5`]} ${lista[`expr $FNO+6`]}" '+%m')
-FILE_MONTH=$(tr -dc '0-9' <<< $FILE_MONTH)
-FILE_MONTH=${FILE_MONTH#0}
-    if ((FILE_MONTH < MONTH_NUMBER));
-    then 
- echo "Removing ${lista[`expr $FNO+8`]}"
-      lftp -u $ftpuser,$ftppass $ftpsite <<EOMYF
+  FILE_MONTH=$(date -d "${lista[$(expr $FNO+5)]} ${lista[$(expr $FNO+6)]}" '+%m')
+  FILE_MONTH=$(tr -dc '0-9' <<<$FILE_MONTH)
+  FILE_MONTH=${FILE_MONTH#0}
+  if ((FILE_MONTH < MONTH_NUMBER)); then
+    echo "Removing ${lista[$(expr $FNO+8)]} cause previous month"
+    lftp -u $ftpuser,$ftppass $ftpsite <<EOMYF
       cd $putdir
-      rm ${lista[`expr $FNO+8`]}
+      rm ${lista[$(expr $FNO+8)]}
       bye
 EOMYF
-    fi
+  fi
 
-   if [ ${lista[`expr $FNO+5`]}=$MM ];
-   then
-     if [[ ${lista[`expr $FNO+6`]#0} -lt $DD ]];
-     then
+  #echo Month ${lista[$(expr $FNO+5)]}
+  #echo MM $MM
+  if [ ${lista[$(expr $FNO+5)]} = $MM ]; then
+    #echo Day ${lista[$(expr $FNO+6)]#0}
+    #echo DD: $DD
+    if [[ ${lista[$(expr $FNO+6)]#0} -lt $DD ]]; then
       # Remove this file
-      echo "Removing ${lista[`expr $FNO+8`]}"
+      echo "Removing ${lista[$(expr $FNO+8)]}"
       lftp -u $ftpuser,$ftppass $ftpsite <<EOMYF
       cd $putdir
-      rm ${lista[`expr $FNO+8`]}
+      rm ${lista[$(expr $FNO+8)]}
       bye
 EOMYF
 
-     fi
-   fi
+    fi
+  fi
 done
